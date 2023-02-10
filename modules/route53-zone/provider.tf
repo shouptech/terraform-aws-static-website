65850c5edc24748aface4e093c5dd356791e79e4
Mike Shoup <mike@shoup.io>
2023-02-10 11:57:53 -0700

    Add version restriction to route 53 module

--- modules/route53-zone/provider.tf
+++ modules/route53-zone/provider.tf
@@ -1,4 +1,7 @@
 terraform {
+  # Module was tested using Terraform 1.3.8. Other versions probably still work.
+  required_version = ">= 1.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
